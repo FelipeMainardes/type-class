0cc7e023f31572cd4ba0c1efcf6bb5822d6329f2
felipe <Flipe.mainardes@hotmail.com>
2023-03-22 02:15:27 -0300

    ts

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -10,12 +10,24 @@ const categoriesRepository= new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const {name, description} = request.body;
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-    categoriesRepository.create({name, description})
+    if (categoryAlreadyExists){
+        return response.status(400).json ({error: "Category already exists!"});
+    }
+
+    categoriesRepository.create({name, description});
+
 
         
 
     return response.status(201).send();
 });
 
+
+categoriesRoutes.get ("/", (request, response) => {
+    const all = categoriesRepository.list();
+    
+    return response.json(all);
+})
 export {categoriesRoutes};
