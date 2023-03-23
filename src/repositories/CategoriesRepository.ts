e33ea2df04e8e868eb2ecdfb2b1713e193596d60
felipe <Flipe.mainardes@hotmail.com>
2023-03-22 22:53:54 -0300

    ts

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,13 +1,10 @@
 import { Category} from "../model/Category"
+import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
 
 
 
-interface ICreateCategoryDTO {
-    name: string;
-    description: string;
-}
 
-class CategoriesRepository{
+class CategoriesRepository implements ICategoriesRepository{
 
     private categories: Category[];
     
@@ -34,6 +31,7 @@ list(): Category[] {
 
 findByName(name: string): Category {
     const category= this.categories.find(category=> category.name ===name);
+
     return category;
 }
 }
